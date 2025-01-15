f408699ca79d1f8b69ece39142f21226a6a5567f
Suman9617 <www.sumanmondal9617@gmail.com>
2025-01-15 21:12:10 +0530

    Add files via upload

---
+++ DSA/FUNCTION/problem1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int length, wirdth;
+    printf("enter the value length \n");
+
+    scanf("%d", &length);
+
+    printf("enter the value wirdth\n");
+
+    scanf("%d",&wirdth);
+
+    printf("the area of the retangle %d\n",length*wirdth);
+    return 0;
+}
